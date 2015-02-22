a6a1b42c33bc51d00a5ccf5553e8410b3062d048
victormblopes <victormblopes@gmail.com>
2015-02-22 13:26:08 -0300

    Trocando arraylist por array primitivo!

--- AV1 Java3/src/Estrutura/Executavel.java
+++ AV1 Java3/src/Estrutura/Executavel.java
@@ -10,26 +10,26 @@ public class Executavel {
 		System.out.println("                    ...::: Consecionária DINUPES :::...\n");
 		
 		System.out.println("Cadastrando carros...\n");
-		lj.adicionarCarro("1001", "Ford", "Novo Fiesta", "Branco", "Sedan", 1.6f ,"Automatico", 60000.00f);
-		lj.adicionarCarro("1001", "Ford", "Novo Fiesta", "Branco", "Sedan", 1.6f ,"Automatico", 60000.00f);
-		lj.adicionarCarro("1002", "Chevrolet", "Celta", "Verde", "Hatch", 1.0f ,"Manual", 10000.00f);
-		lj.adicionarCarro("1003", "Fiat", "Palio", "Prata", "Hatch", 1.4f ,"Manual", 18000.00f);
-		lj.adicionarCarro("1003", "Fiat", "Palio", "Prata", "Hatch", 1.4f ,"Manual", 18000.00f);
-		lj.adicionarCarro("1004", "Ford", "Novo Fiesta", "Vermelho", "Sedan", 1.6f ,"Automatico", 60000.00f);
-		lj.adicionarCarro("1005", "Renault", "Duster", "Preto", "SUV", 2.0f ,"Automatico", 80000.00f);
-		lj.adicionarCarro("1005", "Renault", "Duster", "Preto", "SUV", 2.0f ,"Automatico", 80000.00f);
+		lj.adicionarCarro("1001", "Ford", "Novo Fiesta", "Branco", "Sedan", 1.6f ,"Automatico", 60000.00f, 0);
+		lj.adicionarCarro("1001", "Ford", "Novo Fiesta", "Branco", "Sedan", 1.6f ,"Automatico", 60000.00f, 0);
+		lj.adicionarCarro("1002", "Chevrolet", "Celta", "Verde", "Hatch", 1.0f ,"Manual", 10000.00f, 1);
+		lj.adicionarCarro("1003", "Fiat", "Palio", "Prata", "Hatch", 1.4f ,"Manual", 18000.00f, 2);
+		lj.adicionarCarro("1003", "Fiat", "Palio", "Prata", "Hatch", 1.4f ,"Manual", 18000.00f, 2);
+		lj.adicionarCarro("1004", "Ford", "Novo Fiesta", "Vermelho", "Sedan", 1.6f ,"Automatico", 60000.00f, 3);
+		lj.adicionarCarro("1005", "Renault", "Duster", "Preto", "SUV", 2.0f ,"Automatico", 80000.00f, 4);
+		lj.adicionarCarro("1005", "Renault", "Duster", "Preto", "SUV", 2.0f ,"Automatico", 80000.00f, 4);
 		System.out.println();
 		lj.listarEstoquedeCarros();
 		
 		System.out.println("Cadastrando motos...\n");
-		lj.adicionarMoto("2001", "Honda", "CG 150 Titan", "Branco",  150 , 20, 6000.00f);
-		lj.adicionarMoto("2001", "Honda", "CG 150 Titan", "Branco",  150 , 20, 6000.00f);
-		lj.adicionarMoto("2002", "Yamaha", "Factor YBR 125", "Verde", 125, 25, 5000.00f);
-		lj.adicionarMoto("2003", "Suzuki", "Yes 125", "Prata", 125, 15, 8000.00f);
-		lj.adicionarMoto("2003", "Suzuki", "Yes 125", "Prata", 125, 15, 8000.00f);
-		lj.adicionarMoto("2004", "Yamaha", "XTZ", "Vermelho", 125, 23, 7000.00f);
-		lj.adicionarMoto("2005", "Honda", "Biz", "Preto", 125, 18, 50000.00f);
-		lj.adicionarMoto("2005", "Honda", "Biz", "Preto", 125, 18, 50000.00f);
+		lj.adicionarMoto("2001", "Honda", "CG 150 Titan", "Branco",  150 , 20, 6000.00f, 0);
+		lj.adicionarMoto("2001", "Honda", "CG 150 Titan", "Branco",  150 , 20, 6000.00f, 0);
+		lj.adicionarMoto("2002", "Yamaha", "Factor YBR 125", "Verde", 125, 25, 5000.00f, 1);
+		lj.adicionarMoto("2003", "Suzuki", "Yes 125", "Prata", 125, 15, 8000.00f, 2);
+		lj.adicionarMoto("2003", "Suzuki", "Yes 125", "Prata", 125, 15, 8000.00f, 2);
+		lj.adicionarMoto("2004", "Yamaha", "XTZ", "Vermelho", 125, 23, 7000.00f, 3);
+		lj.adicionarMoto("2005", "Honda", "Biz", "Preto", 125, 18, 50000.00f, 4);
+		lj.adicionarMoto("2005", "Honda", "Biz", "Preto", 125, 18, 50000.00f, 4);
 		System.out.println();
 		lj.listarEstoquedeMotos();
 		
@@ -38,7 +38,7 @@ public class Executavel {
 		lj.pesquisarCarro(null, null, "Novo Fiesta", null, null, 0, null, 0);
 		System.out.println();
 		
-		System.out.println("Pesquisando motos...\n");
+		System.out.println("Pesquisando motocicletas...\n");
 		lj.pesquisarMoto(null, "Honda", null, null, 0, 0, 0);
 		lj.pesquisarMoto(null, "Yamaha", null, null, 0, 0, 0);
 		System.out.println();
@@ -53,22 +53,21 @@ public class Executavel {
 		lj.pesquisarMoto("2014");
 		System.out.println();
 		
-		System.out.println("Salvando o estoque em txt...");
+		System.out.println("Salvando o estoque em txt...\n");
 		lj.gravarEstoque();
 		System.out.println();
 		
-		System.out.println("Esvaziando o estoque...");
+		System.out.println("Esvaziando o estoque...\n");
 		lj.limparEstoque();
 		System.out.println();
 		lj.listarEstoquedeCarros();
 		lj.listarEstoquedeMotos();
 		System.out.println();
 		
-		System.out.println("Recuperando estoque do txt...");
+		System.out.println("Recuperando estoque do txt...\n");
 		lj.recuperarEstoque();
+		System.out.println();
 		lj.listarEstoquedeCarros();
 		lj.listarEstoquedeMotos();
-
 	}
-
 }
--- AV1 Java3/src/Estrutura/Loja.java
+++ AV1 Java3/src/Estrutura/Loja.java
@@ -1,5 +1,6 @@
 package Estrutura;
 
+import java.io.EOFException;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.FileWriter;
@@ -16,8 +17,8 @@ public class Loja
 	
 	private String endereco;
 	private String nome;
-	private ArrayList<Carro> estoqueDeCarros = new ArrayList<Carro>();
-	private ArrayList<Motocicleta> estoqueDeMotos = new ArrayList<Motocicleta>();
+	private Carro[] estoqueDeCarros = new Carro[10];
+	private Motocicleta[] estoqueDeMotos = new Motocicleta[10];
 	
 	public String getEndereco() 
 	{
@@ -38,197 +39,302 @@ public class Loja
 	
 	public boolean estaCadastrado(Carro carro)
 	{
-		if(estoqueDeCarros.contains(carro))
-			return true;
+		for(int i = 0; i < estoqueDeCarros.length; i++)
+		{
+			if(estoqueDeCarros[i] != null)
+			{
+				if(estoqueDeCarros[i].equals(carro))
+				{
+					return true;
+				}
+			}
+		}
 		return false;
 	}
 	
 	public boolean estaCadastrado(Motocicleta moto)
 	{
-		if(estoqueDeMotos.contains(moto))
-			return true;
+		for(int i = 0; i < estoqueDeMotos.length; i++)
+		{
+			if(estoqueDeMotos[i] != null)
+			{
+				if(estoqueDeMotos[i].equals(moto))
+				{
+					return true;
+				}
+			}
+		}
 		return false;
 	}
 	
-	public void adicionarCarro(String chassi, String montadora, String modelo, String cor, String tipo, float motorizacao, String cambio, float preco )
+	public void adicionarCarro(String chassi, String montadora, String modelo, String cor, String tipo, float motorizacao, String cambio, float preco, int index)
 	{
 		
-		Carro c = new Carro();
-		
-		c.setChassi(chassi);
-		c.setMontadora(montadora);
-		c.setModelo(modelo);
-		c.setCor(cor);
-		c.setTipo(tipo);
-		c.setMotorizacao(motorizacao);
-		c.setCambio(cambio);
-		c.setPreco(preco);
-		
-		if(estaCadastrado(c))
+		try
 		{
-			System.out.println("Erro: Carro já cadastrado!");
+			Carro c = new Carro();
+			
+			c.setChassi(chassi);
+			c.setMontadora(montadora);
+			c.setModelo(modelo);
+			c.setCor(cor);
+			c.setTipo(tipo);
+			c.setMotorizacao(motorizacao);
+			c.setCambio(cambio);
+			c.setPreco(preco);
+			
+			if(estaCadastrado(c))
+			{
+				System.out.println("Erro: Carro já cadastrado!");
+			}
+			else
+			{
+				estoqueDeCarros[index] = new Carro();
+				estoqueDeCarros[index].setChassi(chassi);
+				estoqueDeCarros[index].setMontadora(montadora);
+				estoqueDeCarros[index].setModelo(modelo);
+				estoqueDeCarros[index].setCor(cor);
+				estoqueDeCarros[index].setTipo(tipo);
+				estoqueDeCarros[index].setMotorizacao(motorizacao);
+				estoqueDeCarros[index].setCambio(cambio);
+				estoqueDeCarros[index].setPreco(preco);
+				System.out.println("Carro cadastrado com sucesso!");				
+			}
 		}
-		else
+		catch(NullPointerException nl)
 		{
-			estoqueDeCarros.add(c);
-			System.out.println("Carro cadastrado com sucesso!");
+			System.out.println("Erro: "+nl);
 		}
 		
 	}
 	
- 	public void adicionarMoto(String chassi, String montadora, String modelo, String cor, int cilindrada, int capacidadeDoTanque, float preco)
+ 	public void adicionarMoto(String chassi, String montadora, String modelo, String cor, int cilindrada, int capacidadeDoTanque, float preco, int index)
 	{
-		
-		Motocicleta m = new Motocicleta();
-		
-		m.setChassi(chassi);
-		m.setMontadora(montadora);
-		m.setModelo(modelo);
-		m.setCor(cor);
-		m.setCilindrada(cilindrada);
-		m.setCapacidadeDoTanque(capacidadeDoTanque);
-		m.setPreco(preco);
-		
-		if(estaCadastrado(m))
+		try
 		{
-			System.out.println("Erro: Motocicleta já cadastrada!");
+			Motocicleta m = new Motocicleta();
+			
+			m.setChassi(chassi);
+			m.setMontadora(montadora);
+			m.setModelo(modelo);
+			m.setCor(cor);
+			m.setCilindrada(cilindrada);
+			m.setCapacidadeDoTanque(capacidadeDoTanque);
+			m.setPreco(preco);
+			
+			if(estaCadastrado(m))
+			{
+				System.out.println("Erro: Motocicleta já cadastrada!");
+			}
+			else
+			{
+				estoqueDeMotos[index] = new Motocicleta();
+				estoqueDeMotos[index].setChassi(chassi);
+				estoqueDeMotos[index].setMontadora(montadora);
+				estoqueDeMotos[index].setModelo(modelo);
+				estoqueDeMotos[index].setCor(cor);
+				estoqueDeMotos[index].setCilindrada(cilindrada);
+				estoqueDeMotos[index].setCapacidadeDoTanque(capacidadeDoTanque);
+				estoqueDeMotos[index].setPreco(preco);
+				System.out.println("Motocicleta cadastrada com sucesso!");
+			}
 		}
-		else
+		catch(NullPointerException nl)
 		{
-			estoqueDeMotos.add(m);
-			System.out.println("Motocicleta cadastrada com sucesso!");
+			System.out.println("Erro: "+nl);
 		}
+		
 		
 	}
  	
  	public void pesquisarCarro(String chassi, String montadora, String modelo, String cor, String tipo, float motorizacao, String cambio, float preco)
  	{
  		
- 		int aux = 0;
- 		
- 		for (Carro carro : estoqueDeCarros) {
-			if((carro.getChassi()==chassi)||(chassi == null))
-			{
-				if((carro.getMontadora()==montadora)||(montadora == null))
-				{
-					if((carro.getModelo()==modelo)||(modelo == null))
-					{
-						if((carro.getCor()==cor)||(cor == null))
-						{
-							if((carro.getTipo()==tipo)||(tipo == null))
-							{
-								if((carro.getMotorizacao()==motorizacao)||(motorizacao == 0))
-								{
-									if((carro.getCambio()==cambio)||(cambio == null))
-									{
-										if((carro.getPreco() == preco)||(preco == 0))
-										{
-											aux += 1;
-											System.out.println("Chassi: "+carro.getChassi()+" | Montadora: "+carro.getMontadora()+" | Modelo: "+carro.getModelo()+" | Cor: "+carro.getCor()+" | Tipo: "+
-											carro.getTipo()+" | Motorização: "+carro.getMotorizacao()+" | Cambio: "+carro.getCambio()+" | Preço: "+carro.getPreco());
-										}
-									}
-								}
-							}
-						}
-					}
-				}
-			}
-		}
- 		if(aux == 0)
+ 		try
  		{
- 			System.out.println("Carro não encontrado!");
+ 			int aux = 0;
+ 	 		
+ 	 		for (int i = 0; i < estoqueDeCarros.length; i++)
+ 	 		{
+ 	 			if(estoqueDeCarros[i] != null)
+ 	 			{
+ 	 				if((estoqueDeCarros[i].getChassi()==chassi)||(chassi == null))
+ 	 				{
+ 	 					if((estoqueDeCarros[i].getMontadora()==montadora)||(montadora == null))
+ 	 					{
+ 	 						if((estoqueDeCarros[i].getModelo()==modelo)||(modelo == null))
+ 	 						{
+ 	 							if((estoqueDeCarros[i].getCor()==cor)||(cor == null))
+ 	 							{
+ 	 								if((estoqueDeCarros[i].getTipo()==tipo)||(tipo == null))
+ 	 								{
+ 	 									if((estoqueDeCarros[i].getMotorizacao()==motorizacao)||(motorizacao == 0))
+ 	 									{
+ 	 										if((estoqueDeCarros[i].getCambio()==cambio)||(cambio == null))
+ 	 										{
+ 	 											if((estoqueDeCarros[i].getPreco() == preco)||(preco == 0))
+ 	 											{
+ 	 												aux += 1;
+ 	 												System.out.println("Chassi: "+estoqueDeCarros[i].getChassi()+" | Montadora: "+estoqueDeCarros[i].getMontadora()+" | Modelo: "+
+ 	 												estoqueDeCarros[i].getModelo()+" | Cor: "+estoqueDeCarros[i].getCor()+" | Tipo: "+	estoqueDeCarros[i].getTipo()+
+ 	 												" | Motorização: "+estoqueDeCarros[i].getMotorizacao()+" | Cambio: "+ estoqueDeCarros[i].getCambio()+
+ 	 												" | Preço: "+estoqueDeCarros[i].getPreco());
+ 	 											}
+ 	 										}
+ 	 									}
+ 	 								}
+ 	 							}
+ 	 						}
+ 	 					}
+ 	 				}
+ 	 			}
+ 			}
+ 	 		if(aux == 0)
+ 	 		{
+ 	 			System.out.println("Carro não encontrado!");
+ 	 		}
  		}
+ 		catch(NullPointerException nl)
+ 		{
+ 			System.out.println("Erro: "+nl);
+ 		}
+ 		
  	}
  	
  	public void pesquisarMoto(String chassi, String montadora, String modelo, String cor, int cilindrada, int capacidadeDoTanque, float preco)
  	{
  		
- 		int aux = 0;
- 		
- 		for (Motocicleta moto : estoqueDeMotos) {
-			if((moto.getChassi()==chassi)||(chassi == null))
-			{
-				if((moto.getMontadora()==montadora)||(montadora == null))
-				{
-					if((moto.getModelo()==modelo)||(modelo == null))
-					{
-						if((moto.getCor()==cor)||(cor == null))
-						{
-							if((moto.getCilindrada()==cilindrada)||(cilindrada == 0))
-							{
-								if((moto.getCapacidadeDoTanque()==capacidadeDoTanque)||(capacidadeDoTanque == 0))
-								{
-									if((moto.getPreco() == preco)||(preco == 0))
-									{
-										aux += 1;
-										System.out.println("Chassi: "+moto.getChassi()+" | Montadora: "+moto.getMontadora()+" | Modelo: "+moto.getModelo()+" | Cor: "+moto.getCor()+
-										" | Cilindrada: "+moto.getCilindrada()+" | Capacidade do Tanque de Combustível: "+moto.getCapacidadeDoTanque()+" | Preço: "+moto.getPreco());
-									}
-								}
-							}
-						}
-					}
-				}
-			}
-		}
- 		if(aux == 0)
+ 		try
  		{
- 			System.out.println("Moto não encontrada!");
+ 	 		int aux = 0;
+ 	 		
+ 	 		for (int i = 0; i < estoqueDeMotos.length; i++)
+ 	 		{
+ 	 			if(estoqueDeMotos[i] != null)
+ 	 			{
+ 	 				if((estoqueDeMotos[i].getChassi()==chassi)||(chassi == null))
+ 	 				{
+ 	 					if((estoqueDeMotos[i].getMontadora()==montadora)||(montadora == null))
+ 	 					{
+ 	 						if((estoqueDeMotos[i].getModelo()==modelo)||(modelo == null))
+ 	 						{
+ 	 							if((estoqueDeMotos[i].getCor()==cor)||(cor == null))
+ 	 							{
+ 	 								if((estoqueDeMotos[i].getCilindrada()==cilindrada)||(cilindrada == 0))
+ 	 								{
+ 	 									if((estoqueDeMotos[i].getCapacidadeDoTanque()==capacidadeDoTanque)||(capacidadeDoTanque == 0))
+ 	 									{
+ 	 										if((estoqueDeMotos[i].getPreco() == preco)||(preco == 0))
+ 	 										{
+ 	 											aux += 1;
+ 	 											System.out.println("Chassi: "+estoqueDeMotos[i].getChassi()+" | Montadora: "+estoqueDeMotos[i].getMontadora()+" | Modelo: "+
+ 	 											estoqueDeMotos[i].getModelo()+" | Cor: "+estoqueDeMotos[i].getCor()+" | Cilindrada: "+	estoqueDeMotos[i].getCilindrada()+
+ 	 											" | Capacidade do Tanque: "+estoqueDeMotos[i].getCapacidadeDoTanque()+" | Preço: "+estoqueDeMotos[i].getPreco());
+ 	 										}
+ 	 									}
+ 	 								}
+ 	 							}
+ 	 						}
+ 	 					}
+ 	 				}
+ 	 			}	
+ 	 		}
+ 	 		if(aux == 0)
+ 		 	{
+ 		 		System.out.println("Motocicleta não encontrada!");
+ 		 	}
  		}
+ 		catch(NullPointerException nl)
+ 		{
+ 			System.out.println("Erro: "+nl);
+ 		}
+			
  	}
  	
  	public void pesquisarCarro(String chassi)
  	{
- 		int aux = 0;
- 		
- 		for (Carro carro : estoqueDeCarros) {
-			if(carro.getChassi()==chassi)
-			{
-				aux += 1;
-				System.out.println("Chassi: "+carro.getChassi()+" | Montadora: "+carro.getMontadora()+" | Modelo: "+carro.getModelo()+" | Cor: "+carro.getCor()+" | Tipo: "+
-				carro.getTipo()+" | Motorização: "+carro.getMotorizacao()+" | Cambio: "+carro.getCambio()+" | Preço: "+carro.getPreco());
-			}
-		}
- 		if(aux == 0)
+ 		try
  		{
- 			System.out.println("Carro não encontrado!");
+ 	 		int aux = 0;
+ 	 		
+ 	 		for (int i = 0; i < estoqueDeCarros.length; i++) 
+ 	 		{
+ 				if(estoqueDeCarros[i] != null)
+ 				{
+ 					if(estoqueDeCarros[i].getChassi()==chassi)
+ 					{
+ 						aux += 1;
+ 						System.out.println("Chassi: "+estoqueDeCarros[i].getChassi()+" | Montadora: "+estoqueDeCarros[i].getMontadora()+" | Modelo: "+
+ 						estoqueDeCarros[i].getModelo()+" | Cor: "+estoqueDeCarros[i].getCor()+" | Tipo: "+	estoqueDeCarros[i].getTipo()+
+ 						" | Motorização: "+estoqueDeCarros[i].getMotorizacao()+" | Cambio: "+ estoqueDeCarros[i].getCambio()+
+ 						" | Preço: "+estoqueDeCarros[i].getPreco());
+ 					}
+ 				}
+ 	 		}
+			if(aux == 0)
+			{
+				System.out.println("Carro não encontrado!");
+			}
  		}
- 		
+ 		catch(NullPointerException nl)
+ 		{
+ 			System.out.println("Erro: "+nl);
+ 		}
  	}
  	
  	public void pesquisarMoto(String chassi)
  	{
- 		int aux = 0;
- 		
- 		for (Motocicleta moto : estoqueDeMotos) {
-			if(moto.getChassi()==chassi)
-			{
-				aux += 1;
-				System.out.println("Chassi: "+moto.getChassi()+" | Montadora: "+moto.getMontadora()+" | Modelo: "+moto.getModelo()+" | Cor: "+moto.getCor()+
-				" | Cilindrada: "+moto.getCilindrada()+" | Capacidade do Tanque de Combustível: "+moto.getCapacidadeDoTanque()+" | Preço: "+moto.getPreco());
-			}
-		}
- 		if(aux == 0)
+ 		try
  		{
- 			System.out.println("Moto não encontrada!");
+ 	 		int aux = 0;
+ 			for(int i = 0; i < estoqueDeMotos.length; i++)
+ 			{
+ 				if(estoqueDeMotos[i] != null)
+ 				{
+ 					if(estoqueDeMotos[i].getChassi()==chassi)
+ 					{
+ 						aux += 1;
+ 						System.out.println("Chassi: "+estoqueDeMotos[i].getChassi()+" | Montadora: "+estoqueDeMotos[i].getMontadora()+" | Modelo: "+
+						estoqueDeMotos[i].getModelo()+" | Cor: "+estoqueDeMotos[i].getCor()+" | Cilindrada: "+	estoqueDeMotos[i].getCilindrada()+
+						" | Capacidade do Tanque: "+estoqueDeMotos[i].getCapacidadeDoTanque()+" | Preço: "+estoqueDeMotos[i].getPreco());
+ 					}
+ 					
+ 				}
+ 			}
+ 			if(aux == 0)
+			{
+				System.out.println("Motocicleta não encontrada!");
+			}
+ 		}
+ 		catch(NullPointerException nl)
+ 		{
+ 			System.out.println("Erro: "+nl);
  		}
  	}
  	
  	public void listarEstoquedeCarros()
  	{
  		
- 		System.out.println("Listando carros cadastrados...\n");
- 		if(estoqueDeCarros.isEmpty())
+ 		try
  		{
- 			System.out.println("Estoque vazio!");
+ 	 		System.out.println("Listando carros cadastrados...\n");
+ 	 		for( int i = 0; i < estoqueDeCarros.length; i++)
+ 	 		{
+ 	 			if(estoqueDeCarros[i] == null)
+ 	 	 		{
+ 	 	 			System.out.println("Vaga disponível!");
+ 	 	 		}
+ 	 	 		else
+ 	 	 		{
+ 	 	 	 		System.out.println("Chassi: "+estoqueDeCarros[i].getChassi()+" | Montadora: "+estoqueDeCarros[i].getMontadora()+" | Modelo: "+estoqueDeCarros[i].getModelo()+
+ 	 	 	 		" | Cor: "+estoqueDeCarros[i].getCor()+" | Tipo: "+	estoqueDeCarros[i].getTipo()+" | Motorização: "+estoqueDeCarros[i].getMotorizacao()+" | Cambio: "+
+ 	 	 	 		estoqueDeCarros[i].getCambio()+" | Preço: "+estoqueDeCarros[i].getPreco());
+ 	 	 		}
+ 	 		}
  		}
- 		else
+ 		catch(NullPointerException nl)
  		{
- 	 		for (Carro carro : estoqueDeCarros) {
- 				System.out.println("Chassi: "+carro.getChassi()+" | Montadora: "+carro.getMontadora()+" | Modelo: "+carro.getModelo()+" | Cor: "+carro.getCor()+" | Tipo: "+
- 				carro.getTipo()+" | Motorização: "+carro.getMotorizacao()+" | Cambio: "+carro.getCambio()+" | Preço: "+carro.getPreco());
- 			}
+ 			System.out.println("Erro: "+nl);
  		}
  		System.out.println("");
  		
@@ -237,25 +343,42 @@ public class Loja
  	public void listarEstoquedeMotos()
  	{
  		
- 		System.out.println("Listando motos cadastradas...\n");
- 		if(estoqueDeMotos.isEmpty())
+ 		try
  		{
- 			System.out.println("Estoque vazio!");
+ 	 		System.out.println("Listando motocicletas cadastradas...\n");
+ 	 		for( int i = 0; i < estoqueDeMotos.length; i++)
+ 	 		{
+ 	 			if(estoqueDeMotos[i] == null)
+ 	 	 		{
+ 	 	 			System.out.println("Vaga disponível!");
+ 	 	 		}
+ 	 	 		else
+ 	 	 		{
+ 	 	 	 		System.out.println("Chassi: "+estoqueDeMotos[i].getChassi()+" | Montadora: "+estoqueDeMotos[i].getMontadora()+" | Modelo: "+estoqueDeMotos[i].getModelo()+
+ 	 	 	 		" | Cor: "+estoqueDeMotos[i].getCor()+" | Cilindrada: "+	estoqueDeMotos[i].getCilindrada()+" | Capacidade do Tanque: "+
+ 	 	 	 		estoqueDeMotos[i].getCapacidadeDoTanque()+" | Preço: "+estoqueDeMotos[i].getPreco());
+ 	 	 		}
+ 	 		}
  		}
- 		else
+ 		catch(NullPointerException nl)
  		{
- 			for (Motocicleta moto : estoqueDeMotos) {
- 				System.out.println("Chassi: "+moto.getChassi()+" | Montadora: "+moto.getMontadora()+" | Modelo: "+moto.getModelo()+" | Cor: "+moto.getCor()+" | Cilindrada: "
- 				+moto.getCilindrada()+" | Capacidade do Tanque de Combustível: "+moto.getCapacidadeDoTanque()+" | Preço: "+moto.getPreco());
- 			}
+ 			System.out.println("Erro: "+nl);
  		}
  		System.out.println("");
  	}
  	
  	public void limparEstoque()
  	{
- 		estoqueDeCarros.clear();
- 		estoqueDeMotos.clear();
+ 		for(int i = 0; i < estoqueDeCarros.length; i++)
+ 		{
+ 	 		estoqueDeCarros[i] = null;
+ 		}
+	 	System.out.println("Estoque de carros vazio!");
+ 		for(int i = 0; i < estoqueDeMotos.length; i++)
+ 		{
+ 	 		estoqueDeMotos[i] = null;
+ 		}
+ 		System.out.println("Estoque de motocicletas vazio!");
  	}
  	
  	public void gravarEstoque()
@@ -297,7 +420,6 @@ public class Loja
  		
  	}
  	
- 	@SuppressWarnings("null")
 	public void recuperarEstoque()
  	{
  
@@ -305,32 +427,61 @@ public class Loja
 	 	{
 	 		FileInputStream fileIn = new FileInputStream("estoqueDeCarros.txt");
 	 		ObjectInputStream in = new ObjectInputStream(fileIn);
-	 		while(fileIn != null)
+	 		int i = 0;
+	 		while((fileIn != null)&&(i < estoqueDeCarros.length))
 	 		{
 	 			Carro carro = (Carro) in.readObject();
-	 			estoqueDeCarros.add(carro);
+				if(carro != null)
+				{
+					estoqueDeCarros[i] =  new Carro();
+					estoqueDeCarros[i].setChassi(carro.getChassi());
+					estoqueDeCarros[i].setMontadora(carro.getMontadora());
+					estoqueDeCarros[i].setModelo(carro.getModelo());
+					estoqueDeCarros[i].setCor(carro.getCor());
+					estoqueDeCarros[i].setTipo(carro.getTipo());
+					estoqueDeCarros[i].setMotorizacao(carro.getMotorizacao());
+					estoqueDeCarros[i].setCambio(carro.getCambio());
+					estoqueDeCarros[i].setPreco(carro.getPreco());
+				}
+				i += 1;
 	 		}
+	 		System.out.println("Estoque de carros recuperado com sucesso!");
 	 		in.close();
 	 		fileIn.close();
 	 	}
-		catch(IOException | ClassNotFoundException io)
+		catch(IOException | ClassNotFoundException | NullPointerException io)
 		{
+			System.out.println("Erro: "+io);
 		}
- 		
+
  		try
 	 	{
 	 		FileInputStream fileIn = new FileInputStream("estoqueDeMotos.txt");
 	 		ObjectInputStream in = new ObjectInputStream(fileIn);
-	 		while(fileIn != null)
+	 		int i = 0;
+	 		while((fileIn != null)&&(i < estoqueDeMotos.length))
 	 		{
 	 			Motocicleta moto = (Motocicleta) in.readObject();
-	 			estoqueDeMotos.add(moto);
+	 			if(moto != null)
+	 			{
+		 			estoqueDeMotos[i] = new Motocicleta();
+		 			estoqueDeMotos[i].setChassi(moto.getChassi());
+		 			estoqueDeMotos[i].setMontadora(moto.getMontadora());
+		 			estoqueDeMotos[i].setModelo(moto.getModelo());
+		 			estoqueDeMotos[i].setCor(moto.getCor());
+		 			estoqueDeMotos[i].setCilindrada(moto.getCilindrada());
+		 			estoqueDeMotos[i].setCapacidadeDoTanque(moto.getCapacidadeDoTanque());
+		 			estoqueDeMotos[i].setPreco(moto.getPreco());
+	 			}
+				i += 1;
 	 		}
+	 		System.out.println("Estoque de motocicletas recuperado com sucesso!");
 	 		in.close();
 	 		fileIn.close();
 	 	}
-		catch(IOException | ClassNotFoundException io)
+		catch(IOException | ClassNotFoundException | NullPointerException io)
 		{
+			System.out.println("Erro: "+io);
 		}
  		
  	}
